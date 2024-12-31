71414731a60fd51fa841648dbac5f7cec4cffc2b
Aaditya Pyarla <aaditya.pyarla@gmail.com>
2024-12-31 04:15:04 +0000

    Initialized project in QT Creator

---
+++ build/Desktop-Debug/CMakeFiles/QuizMania.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuizMania_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/QuizMania_autogen.dir/ParseCache.txt"
+  "QuizMania_autogen"
+  "CMakeFiles/QuizMania.dir/QuizMania_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/QuizMania.dir/QuizMania_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/QuizMania.dir/main.cpp.o"
+  "CMakeFiles/QuizMania.dir/main.cpp.o.d"
+  "CMakeFiles/QuizMania.dir/mainwindow.cpp.o"
+  "CMakeFiles/QuizMania.dir/mainwindow.cpp.o.d"
+  "QuizMania"
+  "QuizMania.pdb"
+  "QuizMania_autogen/mocs_compilation.cpp"
+  "QuizMania_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuizMania.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Desktop-Debug/CMakeFiles/QuizMania.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for QuizMania.
+# This may be replaced when dependencies are built.
