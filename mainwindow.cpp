95e8e9fb51f079b029b80877e13c112d9a37d556
Aaditya Pyarla <aaditya.pyarla@gmail.com>
2025-01-01 16:37:30 +0000

    Added Live User Input Validation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,119 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <regex>
+#include <string>
+#include <QDebug>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(this, SIGNAL(noEmptyField()), this, SLOT(noEmptyFieldCheck()));
+    connect(ui->email, SIGNAL(textEdited(QString)), this, SLOT(on_email_textEdited(QString)));
+    connect(ui->firstName, SIGNAL(textEdited()), this, SLOT(on_firstName_textEdited()));
+    connect(ui->lastName, SIGNAL(textEdited()), this, SLOT(on_lastName_textEdited()));
+    connect(ui->username, SIGNAL(textEdited()), this, SLOT(on_username_textEdited()));
+    connect(ui->nextButton, SIGNAL(clicked()), this, SLOT(on_nextButton_clicked()));
+    connect(ui->changeEmailButton, SIGNAL(clicked()), this, SLOT(on_changeEmailButton_clicked()));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::noEmptyFieldCheck() {
+    if(m_emailValid && m_firstName && m_lastName && m_userName) {
+        ui->nextButton->setEnabled(true);
+    } else {
+        ui->nextButton->setEnabled(false);
+    }
+}
+
+bool MainWindow::isEmailValid(const std::string& email)
+{
+
+    // Regular expression definition
+    const std::regex pattern(
+        R"((^[A-Za-z0-9]+(?:[._%+-]*[A-Za-z0-9]+)*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$))");
+
+    // Match the string pattern
+    // with regular expression
+    return regex_match(email, pattern);
+}
+
+
+
+void MainWindow::on_email_textEdited(const QString &email)
+{
+    if (isEmailValid(email.toStdString())) {
+        ui->emailLabel->setText("✅ You have entered a valid email.");
+        m_emailValid = true;
+
+    } else {
+        ui->emailLabel->setText("⚠️ Please enter a valid email.");
+        m_emailValid = false;
+    }
+    emit noEmptyFieldCheck();
+}
+
+
+void MainWindow::on_firstName_textEdited()
+{
+
+    if (ui->firstName->text().isEmpty()) {
+        m_firstName = false;
+
+    } else {
+        m_firstName = true;
+    }
+    emit noEmptyFieldCheck();
+}
+
+
+void MainWindow::on_lastName_textEdited()
+{
+    if (ui->lastName->text().isEmpty()) {
+        m_lastName = false;
+
+    } else {
+        m_lastName = true;
+    }
+    emit noEmptyFieldCheck();
+}
+
+
+void MainWindow::on_username_textEdited()
+{
+    if (ui->username->text().isEmpty()) {
+        ui->usernameLabel->setText("⚠️ You have entered a invalid username.");
+        m_userName = false;
+
+    } else {
+        ui->usernameLabel->setText("✅ You have entered a valid username.");
+        m_userName = true;
+    }
+    emit noEmptyFieldCheck();
+}
+
+
+void MainWindow::on_nextButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_changeEmailButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+    ui->emailConfirmCode->clear();
+    ui->email->clear();
+    ui->email->setFocus();
+    m_emailValid = false;
+    emit noEmptyFieldCheck();
+    ui->registerWidget_2->setWindowTitle("User Registeration - QuizMania");
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <string>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -17,7 +17,33 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+
+signals:
+    void noEmptyField(void);
+
+private slots:
+    void on_email_textEdited(const QString &arg1);
+
+    void on_firstName_textEdited();
+
+    void on_lastName_textEdited();
+
+    void noEmptyFieldCheck();
+
+    void on_username_textEdited();
+
+    void on_nextButton_clicked();
+
+    void on_changeEmailButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    bool isEmailValid(const std::string& email);
+    bool m_emailValid = false;
+    bool m_firstName = false;
+    bool m_lastName = false;
+    bool m_userName = false;
 };
 #endif // MAINWINDOW_H
